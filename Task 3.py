77c6be4d2e6030d3f99454ee739a2ab9128ce315
unknown <v.duganov2014@yandex.ru>
2024-11-19 21:29:15 +0300

    editing task3

--- Task 3.py
+++ Task 3.py
@@ -8,7 +8,7 @@ class Student:
         self.grades = {}
 
     def rate_lec(self, lecturer, course, grade):
-        if isinstance(lecturer , Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.lec_grades:
                 lecturer.lec_grades[course] += [grade]
             else:
@@ -16,11 +16,15 @@ class Student:
         else:
             return 'Ошибка'
 
-    def middle_value(self, course):
-        if course in self.grades:
-            return sum(self.grades[course]) / len(self.grades[course])
-        else:
-            return 0
+    def middle_value(self):
+        total_sum = 0
+        total_count = 0
+        for value in self.grades.values():
+            for number in value:
+                total_sum += number
+        for value in self.grades.values():
+            total_count += len(value)
+        return total_sum / total_count
 
     def __str__(self):
         result = (
@@ -29,15 +33,13 @@ class Student:
             f"Фамилия: {self.surname}\n"
             f"Курсы в процесе изучения: {','.join(map(str, self.courses_in_progress))}\n"
             f"Завершенные курсы: Введение в программирование\n"
+            f"Средняя оценка за домашние задания: {self.middle_value()}\n"
         )
-        for course, grades in self.grades.items():
-            average = self.middle_value(course)
-            result += f"Средняя оценка за домашние задания: {average:.2f}\n"
         return result
 
     def __lt__(self, other):
         for course, grades in self.grades.items():
-            return self.middle_value(course) < other.middle_value(course)
+            return self.middle_value() < other.middle_value()
 
 
 class Mentor:
@@ -52,26 +54,28 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.lec_grades = {}
 
-    def middle_value(self, course):
-        if course in self.lec_grades:
-            return sum(self.lec_grades[course]) / len(self.lec_grades[course])
-        else:
-            return 0
+    def middle_value(self):
+        total_sum = 0
+        total_count = 0
+        for value in self.lec_grades.values():
+            for number in value:
+                total_sum += number
+        for value in self.lec_grades.values():
+            total_count += len(value)
+        return total_sum / total_count
 
     def  __str__(self):
         result = (
             f"ЛЕКТОР\n"
             f"Имя: {self.name}\n"
             f"Фамилия: {self.surname}\n"
+            f"Средняя оценка за лекции: {self.middle_value()}\n"
         )
-        for course, grades in self.lec_grades.items():
-            average = self.middle_value(course)
-            result += f"Средняя оценка за лекции: {average:.2f}\n"
         return result
 
     def __lt__(self, other):
         for course, grades in self.lec_grades.items():
-            return self.middle_value(course) < other.middle_value(course)
+            return self.middle_value() < other.middle_value()
 
 
 class Reviewer(Mentor):
@@ -94,25 +98,41 @@ class Reviewer(Mentor):
 
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Roy', 'Jones', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Git']
 
+just_student = Student('Mike', 'Tyson', 'man')
+just_student.courses_in_progress += ['Python']
+just_student.courses_in_progress += ['Git']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Git']
 
+just_reviewer = Reviewer('Once', 'Toldme')
+just_reviewer.courses_attached += ['Python']
+just_reviewer.courses_attached += ['Git']
 
-cool_lecturer = Lecturer('Roy', 'Jones')
+cool_lecturer = Lecturer('The', 'World')
 cool_lecturer.courses_attached += ['Python']
+cool_lecturer.courses_attached += ['Git']
 
+just_lecturer = Lecturer('Isgonna', 'Rollme')
+just_lecturer.courses_attached += ['Python']
+just_lecturer.courses_attached += ['Git']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 9)
+cool_reviewer.rate_hw(best_student, 'Git', 9)
 
+cool_reviewer.rate_hw(just_student, 'Python', 9)
+cool_reviewer.rate_hw(just_student, 'Git', 10)
 
 best_student.rate_lec(cool_lecturer, 'Python', 10)
-best_student.rate_lec(cool_lecturer, 'Python', 9)
+best_student.rate_lec(cool_lecturer, 'Git', 9)
+
+best_student.rate_lec(just_lecturer, 'Python', 10)
+best_student.rate_lec(just_lecturer, 'Git', 9)
 
 is_lt = (best_student < cool_lecturer)
 print(f"Средняя оценка студента за домашние задания ниже, чем средняя оценка лектора: {is_lt}\n")
